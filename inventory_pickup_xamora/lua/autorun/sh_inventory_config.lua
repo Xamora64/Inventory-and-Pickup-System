31005592bb2ed3ec5eb543540bc4c1ffbe3c5753
Xamora64 <49618907+Xamora64@users.noreply.github.com>
2022-09-18 15:35:45 +0200

    Add files via upload

---
+++ inventory_pickup_xamora/lua/autorun/sh_inventory_config.lua
@@ -0,0 +1,73 @@
+config = config or {}
+
+// Max place in inventory ( -1 = no limit )
+config.max = -1
+
+// default button to open the inventory
+config.key_open = KEY_I
+
+// default button to take a weapon or entity
+config.key_take = KEY_T
+
+if CLIENT then
+    // if size of case is relative (same size for all screen)
+    config.relative_item = true 
+    // if relative_item is "true", what size you want ? (default = 1.2)
+    config.relative_size = 1.2
+
+    // size of one case if relative_item is false
+    config.size = 125
+
+    // start x case
+    config.x_item = 0
+
+    // start y case
+    config.y_item = 0
+
+    // gap between each case in x
+    config.gap_x = 10
+
+    // gap between each case in y
+    config.gap_y = 20
+
+elseif SERVER then
+    // System pickup allow
+    config.pickup = true
+
+    // press for a long time to Use Button placed on the inventory
+    config.long_time_use = true
+
+    // timer to wait when he press to the button for take
+    config.timer_take = 0
+
+    // distance to take (default = 0.25)
+    config.distance = 0.25
+
+    // Allow message send to the player
+    config.message = true
+
+    // if player die his inventory is drop
+    config.keep_inventory = false 
+    // if keep_inventory is false, drop the inventory
+    config.drop_inventory = true
+    config.model_inventory = "models/props_c17/suitcase001a.mdl"
+    // if you change the model you maybe need to increase or reduce this
+    config.height_spawn = 10
+
+    // if player can take entity
+    config.can_take_entity = true 
+
+    // if player can take weapon
+    config.can_take_weapon = true 
+
+    // if play can take two same weapon in hot bar weapon
+    config.can_take_same_weapon = true 
+
+    // Blacklist weapon in inventory by classname
+    // ex: config.blacklist_weapon = {"weapon_357", "weapon_crossbow"}
+    config.blacklist_weapon = {""}
+
+    // Blacklist entity in inventory by classname
+    config.blacklist_entity = {""}
+
+end
---
+++ inventory_pickup_xamora/lua/autorun/sh_inventory_xamora.lua
@@ -0,0 +1,30 @@
+MsgC(Color(50, 0, 180), "~[Loading Inventory]~\n")
+
+if SERVER then
+
+elseif CLIENT then
+
+end
+
+inventory = inventory or {}
+inventory.Items = {}
+inventory.UseTypes = {
+    weapon = function(itemData, ply)
+        if ply:HasWeapon(itemData.classname) then ply:InvLog("You already have the weapon") return false end
+        ply.give_menu = true
+        ply:Give(itemData.classname)
+        return true
+    end,
+    model = function(itemData, ply)
+        ply:SetModel(itemData.model)
+        return false
+    end,
+    entity = function(itemData, ply)
+        local ent = ents.Create(itemData.classname)
+        ent:SetPos(ply:GetPos() + Vector(0, 0, 20))
+        ent:SetAngles(Angle(0, 0, 0))
+        ent:Spawn()
+        ply.give_menu = true
+        return true
+    end,
+}
